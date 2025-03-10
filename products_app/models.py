715275b0bbd43a3e670b0fc72c7ad2779d7aa60b
abhishekpythoninmakes <abhishekar3690@gmail.com>
2025-03-10 14:04:15 +0530

    equaion calculation

--- products_app/models.py
+++ products_app/models.py
@@ -66,6 +66,7 @@ class Product(models.Model):
     size = models.CharField(max_length=100, choices=sizes_available, default='cm',help_text="🛠 Select the measurement unit")
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     status = models.ForeignKey(Product_status, on_delete=models.CASCADE, null=True, blank=True)
+    amazon_url = models.URLField(null=True, blank=True)
     def __str__(self):
         return self.name if self.name else "Unnamed Product"
 
